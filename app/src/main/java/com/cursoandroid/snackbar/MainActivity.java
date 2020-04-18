acca37c4e6be258410f9843365734f2ad778d5a9
LUCAS ANDRÉ <lucas_andre_spfc@hotmail.com>
2020-04-18 20:14:43 -0300

    Commit - Create button, create attribute private buttonAbrir and method setOnclickListener

--- app/src/main/java/com/cursoandroid/snackbar/MainActivity.java
+++ app/src/main/java/com/cursoandroid/snackbar/MainActivity.java
@@ -3,12 +3,33 @@ package com.cursoandroid.snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAbrir;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAbrir = findViewById(R.id.buttonAbrir);
+
+        buttonAbrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
+    public void abrirSnackbar(View view){
+
+    }
+
+
 }
